29966e9fff1846b8a1c14b2e6eacbf8e6832903d
Rodionov Sergey <maximus777.999@mail.ru>
2018-04-27 16:27:07 +0300

    Набросал разметку, добавил зависимость room

---
+++ app/src/main/java/com/justapp/photofeed/PhotoViewActivity.java
@@ -0,0 +1,9 @@
+package com.justapp.photofeed;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * @author Sergey Rodionov
+ */
+public class PhotoViewActivity extends AppCompatActivity {
+}
